f7e828e804cbeb85a435628331a65b0b91312712
Admin <jithinks112@gmail.com>
2021-02-16 21:14:31 +0530

    Added no source map flag

--- dist/strive.js
+++ dist/strive.js
@@ -237,7 +237,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45603" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38219" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -414,4 +414,3 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.ts"], null)
-//# sourceMappingURL=/strive.js.map
