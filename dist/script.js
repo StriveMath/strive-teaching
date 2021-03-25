f8fcec6f7fb45e27aed05733d4849c3ac8204264
tamirshklaz <shklazt@gmail.com>
2021-03-25 14:31:21 +0800

    Reformat code

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,7 @@ let p1;
 function setup() {
 	createCanvas(800, 800)
 	p1 = Strive.createMovableCircle(100, 100, 20)
+	angleMode(DEGREES)
 }
 
 
@@ -10,6 +11,7 @@ function draw() {
 	background("black")
 	Strive.translate(width / 2, height / 2)
 	Strive.scale(2, -2)
+	Strive.rotate(45)
 	Strive.drawTickAxes()
 	p1.draw()
 
