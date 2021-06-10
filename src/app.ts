a45441b4452ca613bd17e5ad4261e225ce3447b4
Teri <50551345+teriaiw@users.noreply.github.com>
2021-06-10 19:38:16 +0800

    Add easyStart function

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,13 @@ window.setup = () => {
 };
 // @ts-ignore
 window.draw = () => {
-  background("black");
-  stroke("white")
-  Strive.translate(width/2, height/2)
-  Strive.arrow(100,100,200,200)
-  Strive.scale(1,-1)
-  Strive.drawTickAxes();
-  fill("white")
-  Strive.text("Hello World", 100,100)
+  //background("black");
+  // stroke("white")
+  // Strive.translate(width/2, height/2)
+  // Strive.arrow(100,100,200,200)
+  // Strive.scale(1,-1)
+  // Strive.drawTickAxes();
+  // fill("white")
+  // Strive.text("Hello World", 100,100)
+  Strive.easyStart();
 };
